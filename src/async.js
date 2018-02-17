89087e6fb867a84174abf08a6f9b6b33475ebd46
Larry Botha <larry@fixate.it>
2018-02-17 10:17:50 +0200

    add a class with an async function

--- src/async.js
+++ src/async.js
@@ -18,10 +18,27 @@ const showGithubUserAsync = async handle => {
   const response = await fetch(url);
 
   return await response.json();
+};
+
+// we can also defined async class methods
+class GitHubApiClient {
+  // we prepend the class method with async
+  async fetchUser(handle) {
+    // and perform the usual async / await blocking here
+    const url = getUrl(handle);
+    const response = await fetch(url);
+
+    return await response.json();
+  }
 }
 
 // await can't be used at the top-level of a file - it's a syntax error, as
 // it can only reside inside a function
 // To get around this, we can use the IIFE pattern
 (async () => {
+  // we can instantiate the class
+  const client = new GitHubApiClient();
+  // and then execute the async function as usual again
+  const user = await client.fetchUser('larrybotha');
+  logUser(user);
 })();
