200d9137754f5470aa965df689ce7d64ecdca6d9
Larry Botha <larry@fixate.it>
2018-02-17 10:17:08 +0200

    annotate different ways of creating async function expressions

--- src/async.js
+++ src/async.js
@@ -7,7 +7,13 @@ const logUser = ({name, location}) => {
   console.log(location);
 };
 
-async function showGithubUserAsync(handle) {
+// async function showGithubUserAsync(handle) {
+
+// Write the function as an expression
+// const showGithubUserAsync = async function(handle) {
+
+// write the function as an expression using arrow
+const showGithubUserAsync = async handle => {
   const url = getUrl(handle);
   const response = await fetch(url);
 
