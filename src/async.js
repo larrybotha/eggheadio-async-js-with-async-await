59f0ddaceac4ab9bae30fb840940c2d9fd60eefd
Larry Botha <larry@fixate.it>
2018-02-18 12:59:30 +0200

    prepare 04

--- src/async.js
+++ src/async.js
@@ -7,38 +7,11 @@ const logUser = ({name, location}) => {
   console.log(location);
 };
 
-// async function showGithubUserAsync(handle) {
-
-// Write the function as an expression
-// const showGithubUserAsync = async function(handle) {
-
-// write the function as an expression using arrow
-const showGithubUserAsync = async handle => {
+const fetchGithubUser = async handle => {
   const url = getUrl(handle);
   const response = await fetch(url);
 
   return await response.json();
 };
 
-// we can also defined async class methods
-class GitHubApiClient {
-  // we prepend the class method with async
-  async fetchUser(handle) {
-    // and perform the usual async / await blocking here
-    const url = getUrl(handle);
-    const response = await fetch(url);
-
-    return await response.json();
-  }
-}
-
-// await can't be used at the top-level of a file - it's a syntax error, as
-// it can only reside inside a function
-// To get around this, we can use the IIFE pattern
-(async () => {
-  // we can instantiate the class
-  const client = new GitHubApiClient();
-  // and then execute the async function as usual again
-  const user = await client.fetchUser('larrybotha');
-  logUser(user);
-})();
+fetchGithubUser('larrybotha').then(logUser);
