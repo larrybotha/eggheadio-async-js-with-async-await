e515970c4f017654d4c2728b1e5496a3d3a7d67e
Larry Botha <larry@fixate.it>
2018-02-18 13:48:54 +0200

    prepare file

--- src/async.js
+++ src/async.js
@@ -7,45 +7,12 @@ const logUser = ({name, location}) => {
   console.log(location);
 };
 
-const fetchGithubUser = async handle => {
+const fetchfromGithub = async handle => {
   const url = getUrl(handle);
   const response = await fetch(url);
-  const body = await response.json();
-
-  if (response.status !== 200) {
-    // an async function will automatically return a rejected promise
-    // whenever an error is thrown
-    // This allows us to chain a catch onto the call-site of the async
-    // function
-    throw Error(body.message);
-  }
-
-  return body;
+  return await response.json();
 };
 
-// what's being done here is not possible with Promises
-const showGithubUser = async handle => {
-  // in a Promise chain the callbacks are called asynchronously, so we
-  // can't use try-catch blocks
-  // With async functions we get that power back
-  try {
-    // asynchronously get the user
-    const user = await fetchGithubUser(handle);
-    // if we get a success response, then log the user
-    logUser(user);
-    // but if an error is thrown from the async function we called above,
-    // then handle it
-  } catch (err) {
-    console.log(`Error: ${err.message}`);
-  }
-};
+async function showUserAndRepos(handle) {}
 
-fetchGithubUser('notreallyauser')
-  .then(res => console.log(res))
-  // we can use catch in a Promise chain because async functions automatically
-  // return a rejected promise when an error is thrown
-  .catch(err => console.error(`Error: ${err.message}`));
-
-// use our async function which has the added benefit of a try-catch block
-showGithubUser('notreallyauser');
-showGithubUser('larrybotha');
+showUserAndRepos('larrybotha');
