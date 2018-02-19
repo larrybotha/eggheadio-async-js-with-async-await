6a722693e9c8a966536dfeef14910257080d47fd
Larry Botha <larry@fixate.it>
2018-02-19 08:21:52 +0200

    add notes on Promise.all

--- src/async.js
+++ src/async.js
@@ -10,6 +10,21 @@ const fetchfromGithub = async handle => {
   return await response.json();
 };
 
-async function showUserAndRepos(handle) {}
+async function showUserAndRepos(handle) {
+  // Promise.all takes a sequence of Promises, executes them concurrently, and
+  // then returns a single promise if they all resolve, else rejects
+  // if one of them doesn't resolve
+  // Promise.all aggregates the results of multiple promises
+  // Furthermore, if any of the Promises are rejected, then Promise.all will
+  // reject immediately - it doesn't wait for the remaining Promises to resolve
+  // or reject
+  const [user, repos] = await Promise.all([
+    fetchfromGithub(handle),
+    fetchfromGithub(`${handle}/repos`),
+  ]);
+
+  log(user.name);
+  log(repos.length);
+}
 
 showUserAndRepos('larrybotha');
