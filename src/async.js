44224d5fb63544733f02ce99574c1aacaee58ec4
Larry Botha <larry@fixate.it>
2018-02-19 08:17:02 +0200

    prepare file

--- src/async.js
+++ src/async.js
@@ -10,33 +10,6 @@ const fetchfromGithub = async handle => {
   return await response.json();
 };
 
-async function showUserAndRepos(handle) {
-  // the problem here is that the requests are made sequentially, and not in
-  // parallel
-  // The second request waits for the first request to succeed before executing
-  // If each request takes .5s, the two requests will take 1s
-  // Analogy: boiling eggs. Boiling eggs in sequence, vs in parallel
-  // const user = await fetchfromGithub(handle);
-  // const repos = await fetchfromGithub(`${handle}/repos`);
-
-  // To get around this, we can execute the calls immediately, and assign the
-  // returned promises to variables. This way, the requests are fired off in
-  // parallel, and we can then wait for the responses.
-  const userPromise = fetchfromGithub(handle);
-  const reposPromise = fetchfromGithub(`${handle}/repos`);
-
-  // after assigning and executing our async requests, we can now assign the
-  // responses to variables using await.
-  // Because both Promises are already pending by this stage, they can be
-  // resolved concurrently, and we're not waiting for one to finish before the
-  // next begins
-  const user = await userPromise;
-  const repos = await reposPromise;
-
-  // we are blocking execution above using await - only once both have resolved
-  // will we get here
-  log(user.name);
-  log(repos.length);
-}
+async function showUserAndRepos(handle) {}
 
 showUserAndRepos('larrybotha');
