feedaed66b3f5e898e329b12ae0da08857803643
Larry Botha <larry@fixate.it>
2018-02-19 08:30:17 +0200

    demonstrate the await receiving a value is the same as an immediately resolved Promise

--- src/async.js
+++ src/async.js
@@ -1,7 +1,16 @@
 
 const log = (...args) => console.log(args);
 
+async function main() {
+  // if we pass a value directly to await, then await returns an immediately
+  // resolved Promise
+  const x = await 42;
+  // the above is the equivalent of the following line
+  const y = await Promise.resolve(42);
 
+  log(x);
+  log(y);
+}
 
 }
 
