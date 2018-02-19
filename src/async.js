c0688de9571bec21ba24055ed0881cf18aa7aefb
Larry Botha <larry@fixate.it>
2018-02-19 08:32:19 +0200

    prepare file

--- src/async.js
+++ src/async.js
@@ -1,28 +1,9 @@
-const Bluebird = require('bluebird');
-
-const log = (...args) => console.log(args);
-
-async function main() {
-  // if we pass a value directly to await, then await returns an immediately
-  // resolved Promise
-  const x = await 42;
-  // the above is the equivalent of the following line
-  const y = await Promise.resolve(42);
-
-  log(x);
-  log(y);
+function* someGen() {
+  yield 1;
+  yield 2;
+  yield 3;
 }
 
-async function main2() {
-  log('Working...');
-  // Bluebird.delay returns an object that is a thenable
-  // There is an implicit Promise.resolve wrapped around values preceded
-  // by the await keyword.
-  // If the argument passed to Promise.resolve has a .then method the final
-  // Promise will follow that thenable to its final state
-  await Bluebird.delay(2000);
-  log('done');
-}
+function main() {}
 
-// main();
-main2();
+main();
