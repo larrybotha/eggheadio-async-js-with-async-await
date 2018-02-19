de59671d34a64fd5a36bc4a57d4aaa34f7ed64e8
Larry Botha <larry@fixate.it>
2018-02-19 08:30:38 +0200

    demonstrate how await handles values that are thenables

--- src/async.js
+++ src/async.js
@@ -1,3 +1,4 @@
+const Bluebird = require('bluebird');
 
 const log = (...args) => console.log(args);
 
@@ -12,5 +13,16 @@ async function main() {
   log(y);
 }
 
+async function main2() {
+  log('Working...');
+  // Bluebird.delay returns an object that is a thenable
+  // There is an implicit Promise.resolve wrapped around values preceded
+  // by the await keyword.
+  // If the argument passed to Promise.resolve has a .then method the final
+  // Promise will follow that thenable to its final state
+  await Bluebird.delay(2000);
+  log('done');
 }
 
+// main();
+main2();
